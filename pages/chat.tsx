3881b1d9e59406bb579cfbda04600066c525af0e
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-11-15 05:33:41 +0100

    Initial commit

---
+++ pages/chat.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import Container from 'components/Container';
+
+const NeueSeite: React.FC = () => {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const iframe = document.getElementById('themeChangerFrame') as HTMLIFrameElement;
+
+    if (iframe) {
+      iframe.contentWindow!.postMessage({ theme: theme }, '*');
+    }
+  }, [theme]);
+
+  return (
+    <Container>
+      <h1>Hallo von der neuen Seite!</h1>
+      <iframe id="themeChangerFrame" src="https://togetherai-einfachllamistral.hf.space" style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        border: 0
+      }}></iframe>
+    </Container>
+  );
+};
+
+export default NeueSeite;
